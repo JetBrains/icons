02c0c8858b70f3a1bcd81d280be822a30fa27a7a
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2021-08-02 20:09:03 +0200

    feat([publish]): add .d.ts files

--- scripts/generate-exports.js
+++ scripts/generate-exports.js
@@ -4,9 +4,14 @@ const path = require('path');
 const SRC_DIR = path.resolve(__dirname, '../');
 const TARGET_DIR = path.resolve(__dirname, '../');
 
-function excapeSource(src) {
+function escapeSource(src) {
   return src.replace(/'/g, "\\'");
 }
+
+const dtsSource =
+`declare const source: string;
+export = source;
+`
 
 function generate() {
   console.log('Generating js exports...')
@@ -22,10 +27,14 @@ function generate() {
       const jsFileName = filename.replace('.svg', '.js');
       
       const fileContent = fs.readFileSync(`${SRC_DIR}/${filename}`, 'utf-8');
-      const source = `module.exports = '${excapeSource(fileContent)}';`;
+      const source = `module.exports = '${escapeSource(fileContent)}';`;
       
       fs.writeFileSync(path.join(TARGET_DIR, jsFileName), source);
       console.log(`Generated ${jsFileName}`)
+
+      const dtsFileName = filename.replace('.svg', '.d.ts');
+      fs.writeFileSync(path.join(TARGET_DIR, dtsFileName), dtsSource);
+      console.log(`Generated ${dtsFileName}`)
     });
 
     console.log('JS exports generated successfully');
